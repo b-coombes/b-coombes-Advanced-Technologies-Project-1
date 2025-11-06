1d83bc6a93d09e78f65984c0319e849c67e85e8c
Benjamin Coombes <benjamin2.coombes@live.uwe.ac.uk>
2025-11-06 13:34:30 +0000

    Door added
    Proximity based door, need to change its activation to voice command

---
+++ Proximity Based Vocalised Abilities/Assets/MadeAssets/Door/Door.cs
@@ -0,0 +1,87 @@
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+
+    public int moveBy;
+    public float speed = 1.0f;
+
+    public bool moving = false;
+    public bool opening = true;
+    private Vector3 startPos;
+    private Vector3 endPos;
+    private float delay = 0.0f;
+
+
+
+    void Start()
+    {
+        startPos = transform.position;
+        endPos = startPos;
+        endPos.y += moveBy;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (moving)
+        {
+            if (opening)
+            {
+                MoveDoor(endPos);
+            }
+            else
+            {
+                MoveDoor(startPos);
+            }
+        }      
+    }
+
+    void MoveDoor(Vector3 goalPos)
+    {
+        float dist = Vector3.Distance(transform.position, goalPos);
+        if (dist > .1f)
+        {
+            transform.position = Vector3.Lerp(transform.position, goalPos, speed * Time.deltaTime);
+        }
+        else
+        {
+            if (opening)
+            {
+                delay += Time.deltaTime;
+                if (delay > 1.5f)
+                {
+                    opening = false;
+                }
+            }
+            else
+            {
+                moving = false;
+                opening = true;
+            }
+        }
+    }
+
+    public bool Moving
+    {
+        get { return moving; }
+        set { moving = value; }
+    }
+
+
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.transform.gameObject.name == "Player")
+        {
+            print("check1");
+            if (moving == false)
+            {
+                print("check2");
+                moving = true;
+            }
+        }
+    }
+
+}
+
